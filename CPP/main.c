7bb2ed10bcf2f4a063df02b38875862dddd4d51b
Zackori Cui <zackory137c@gmail.com>
2020-02-09 18:42:18 -0800

    Initial Commit

---
+++ CPP/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CPP
+//
+//  Created by Zackori Cui on 2/9/20.
+//  Copyright © 2020 Zackori Cui. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
